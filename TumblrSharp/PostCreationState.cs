927f757cd3578ed4f3d8422e632b4416f8b6b648
Edward Roozeboom <info@edwardroozeboom.com>
2014-04-08 14:47:11 +0200

    Initial code checkin, version 1.0.0.0

---
+++ TumblrSharp/PostCreationState.cs
@@ -0,0 +1,29 @@
+﻿
+namespace DontPanic.TumblrSharp
+{
+	/// <summary>
+	/// Defines the creation state of a post.
+	/// </summary>
+	public enum PostCreationState
+	{
+		/// <summary>
+		/// The post will be created as published.
+		/// </summary>
+		Published = 0,
+
+		/// <summary>
+		/// The post will be created as draft.
+		/// </summary>
+		Draft = 1,
+
+		/// <summary>
+		/// The post will be created queued.
+		/// </summary>
+		Queue = 2,
+
+		/// <summary>
+		/// The post will be created as private.
+		/// </summary>
+		Private = 3
+	}
+}
---
+++ TumblrSharp/TumblrErrorResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace DontPanic.TumblrSharp
+{
+    internal class TumblrErrorResponse
+    {
+        [JsonProperty(PropertyName = "errors")]
+        public string[] Errors { get; set; }
+    }
+}
